e62d06ec58bbcebdcdff2ddde74a2dadae663136
Peter Schaedler <pwschaedler@gmail.com>
2016-12-15 13:55:42 -0500

    Commenting

--- resolution/static/resolution.js
+++ resolution/static/resolution.js
@@ -6,7 +6,7 @@
 
 "use strict";
 
-// Detect Browser
+// Detect browser
 var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 var isFirefox = typeof InstallTrigger !== 'undefined';
 var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
@@ -16,12 +16,20 @@ var isChrome = !!window.chrome && !!window.chrome.webstore;
 var isBlink = (isChrome || isOpera) && !!window.CSS;
 var isTor = (isFirefox) && ((new Date().getTimezoneOffset()) === 0) && (navigator.plugins.length === 0) && (window.innerHeight === window.outerHeight);
 
+/*
+ * Returns boolean.
+ * Checks if user is on mobile device.
+ */
 var mobilecheck = function mobilecheck() {
   var check = false;
   (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
   return check;
 };
 
+/*
+ * Returns void.
+ * Creates a cookie with the given name and value.
+ */
 var createCookie = function createCookie(name,value,days) {
 	if (days) {
 		var date = new Date();
@@ -32,6 +40,10 @@ var createCookie = function createCookie(name,value,days) {
 	document.cookie = name+"="+value+expires+"; path=/";
 }
 
+/*
+ * Returns string.
+ * Gets the value of the cookie with the given name or null.
+ */
 var readCookie = function readCookie(name) {
 	var nameEQ = name + "=";
 	var ca = document.cookie.split(';');
@@ -43,16 +55,24 @@ var readCookie = function readCookie(name) {
 	return null;
 }
 
+/*
+ * Returns void.
+ * The main function of our algorithm.
+ * Determines browser via if-statements and calculates resolution accordingly.
+ */
 var onresize = function onresize() {
 	var hres, vres;
 	var pixelRatio = 1.0;
 	var lastCookieChange = new Date('2016-10-28T14:07:00'); // date since last change to cookie policy
 
+	// All mobile browsers
 	if (mobilecheck()) {
 		pixelRatio = window.devicePixelRatio;
-		hres = Math.round(screen.width * pixelRatio);
+		hres = Math.round(screen.width * pixelRatio); // scaling resolution by zoom factor (pixelRatio)
 		vres = Math.round(screen.height * pixelRatio);
 	}
+
+	// Firefox
 	else if (isFirefox) {
 		pixelRatio = window.devicePixelRatio;
 
@@ -64,13 +84,15 @@ var onresize = function onresize() {
 		}
 		pixelRatio /= readCookie("initialRatio");
 
-		hres = Math.round(screen.width * pixelRatio);
+		hres = Math.round(screen.width * pixelRatio); // scaling resolution by zoom factor (pixelRatio)
 		vres = Math.round(screen.height * pixelRatio);
 	}
+
+	// Internet Explorer / Edge
 	else if (isIE || isEdge) {
 		var xzoom = screen.deviceXDPI / screen.logicalXDPI;
 		var yzoom = screen.deviceYDPI / screen.logicalYDPI;
-		hres = Math.round(screen.width * xzoom);
+		hres = Math.round(screen.width * xzoom); // scaling resolution by zoom factor (x/y zoom)
 		vres = Math.round(screen.height * yzoom);
 		// Sometimes rounding error would cause it to be 1 pixel off
 		if (hres % 10 == 1 || hres % 10 == 9) {
@@ -80,9 +102,10 @@ var onresize = function onresize() {
 			vres = Math.round(vres / 10) * 10;
 		}
 	}
+
+	// Chrome / Opera / Safari / other WebKit browsers
 	else {
 		pixelRatio = window.devicePixelRatio;
-
 		// To account for people adjusted screen resolutions because of Windows internal zoom
 		var cookieSetDate = new Date(readCookie("dateSet")); // date cookie was last stored at
 		if (readCookie("initialRatio") === null || cookieSetDate < lastCookieChange) {
@@ -90,22 +113,27 @@ var onresize = function onresize() {
 			var date = new Date();
 			createCookie("dateSet", date.toGMTString(), 365);
 		}
-
-		hres = screen.width * readCookie("initialRatio");
+		hres = screen.width * readCookie("initialRatio"); // scaling resolution by zoom factor (value in cookie)
 		vres = screen.height * readCookie("initialRatio");
 	}
 
+	// Only posts data to database if not Tor Browser, to avoid excessive incorrect data
 	if (!isTor) {
 		createForm('/insertResolution',{'hres':hres,'vres':vres, 'pixel_density':readCookie("initialRatio") , 'cookie': document.cookie, csrfmiddlewaretoken:document.getElementsByName("csrfmiddlewaretoken")[0].value});
 		SubForm();
 	}
 	
+	// Display numbers to web page
 	document.getElementById("center").innerHTML = hres + " x " + vres;
 	if (isIE) {
 		document.getElementById("center").innerHTML += "\nClick to refresh";
 	}
 }
 
+/*
+ * Event handler for page load.
+ * Runs our main function (onresize) at least once and sets other event handlers.
+ */
 onload = function() {
 	if (isIE) {
 		document.getElementById("center").onclick = onresize;
@@ -115,21 +143,28 @@ onload = function() {
 	}
 	onresize();
 };
-	
-function SubForm (){
+
+/*
+ * Returns void.
+ * Sends POST request to database to add data.
+ */
+function SubForm() {
     $.ajax({
 	    url:'/insertResolution',
 		type:'post',
 		data:$('form').serialize(),
-		success:function(){
-		console.log("posted once");
+		success:function() {
+			console.log("posted once");
 	    }
 	});
 }
 
+/*
+ * Returns void.
+ * Helps with creation of POST request for database and back-end using a hidden form.
+ */
 function createForm(path, params, method) {
     method = method || "post"; // Set method to post by default if not specified.
-
 
     // The rest of this code assumes you are not using a library.
     // It can be made less wordy if you use one.
@@ -137,15 +172,15 @@ function createForm(path, params, method) {
     form.setAttribute("method", method);
     form.setAttribute("action", path);
     
-    for(var key in params) {
-        if(params.hasOwnProperty(key)) {
+    for (var key in params) {
+        if (params.hasOwnProperty(key)) {
             var hiddenField = document.createElement("input");
             hiddenField.setAttribute("type", "hidden");
             hiddenField.setAttribute("name", key);
             hiddenField.setAttribute("value", params[key]);
-	    //	    form.setAttribute("target", "/");
+	        // form.setAttribute("target", "/");
             form.appendChild(hiddenField);
-	}
+		}
     }
 
     document.body.appendChild(form);
